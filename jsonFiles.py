97ff1989886fd1147ee53e652e80bbe88d28bdfe
RealyStupid <umar.yousufi@gmail.com>
2026-02-20 09:46:02 -0600

    Use Python dict and pretty-print JSON
    Replace the hardcoded JSON string with a native Python dict that includes additional fields (married, divorced, children, pets, cars). Remove json.loads usage and instead serialize/pretty-print the dict using json.dumps(..., indent=5), outputting the formatted JSON instead of printing a single field.

--- jsonFiles.py
+++ jsonFiles.py
@@ -1,10 +1,16 @@
 import json
 
-#some data
-x = '{"name": "John", "age": 30, "city": "New York"}'
+x = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann","Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
 
-#parse x:
-y = json.loads(x)
-
-#the result is a Python dictionary:
-print(y["age"])
+print(json.dumps(x, indent=5))
